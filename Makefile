561dfd9307b70e48f93174151edd5e8646ea33cc
Marek <mkukacka@gmail.com>
2018-01-01 23:27:31 +0100

    created makefiles to work with GNU Make, added Clean task, minor fixes

--- Makefile
+++ Makefile
@@ -1,16 +1,14 @@
-#http://msdn.microsoft.com/en-us/library/dd9y37ha(v=VS.80).aspx
 
-!INCLUDE <lynxcc65.mak>
+include Makefile_lynxcc65.mk
 
 target = game.lnx
-objects = lynx-160-102-16.o lynx-stdjoy.o \
-					game.o
+objects = lynx-160-102-16.o lynx-stdjoy.o game.o
 
 all: $(target)
 
 $(target) : $(objects)
 	$(CL) -t $(SYS) -o $@ $(objects) lynx.lib 
-	
+
 clean:
 	$(RM) *.tgi
 	$(RM) *.s
--- Makefile_lynxcc65.mk
+++ Makefile_lynxcc65.mk
@@ -1,30 +1,24 @@
-CC65="D:\workspace-lynx\CC65"
+
+CC65=D:\workspace-lynx\CC65
 CC65_BIN=$(CC65)\bin
 CC65_INC=$(CC65)\include
 CC65_ASMINC=$(CC65)\asminc
 CC65_TOOLS=$(CC65)\wbin
 
-BUILDDIR=$(MAKEDIR)\$(BUILD)
-ODIR=$(MAKEDIR)\obj
+CP=$(CC65_TOOLS)\cp
+RM=$(CC65_TOOLS)\rm -f
+CO=$(CC65_BIN)\co65
+CC=$(CC65_BIN)\cc65
+AS=$(CC65_BIN)\ca65
+CL=$(CC65_BIN)\cl65
+SP=$(CC65_BIN)\sp65
+SPRPCK=sprpck
+ECHO=$(CC65_TOOLS)\echo
+TOUCH=$(CC65_TOOLS)\touch
 
 .SUFFIXES : .c .s .o .asm .bmp .pal .spr
 
-OBJ=debug
-
-# We are compiling for Atari Lynx
 SYS=lynx
-
-# These are the names of the tools we use
-CO=co65
-CC=cc65
-AS=ca65
-AR=ar65
-CL=cl65
-SPRPCK=sprpck
-CP=copy
-RM=rm
-ECHO=echo
-TOUCH=touch
 
 CODE_SEGMENT=CODE
 DATA_SEGMENT=DATA
@@ -48,8 +42,6 @@ CFLAGS=-I . -t $(SYS) --add-source -O -Or -Cl -Os
 	$(AS) -t $(SYS) -I $(CC65_ASMINC) -o $@ $(AFLAGS) $<
 
 # Rule for making a *.o file out of a *.c file
-#{}.c{$(ODIR)}.o:
-
 .c.o:
 	$(CC) $(SEGMENTS) $(CFLAGS) $<
 	$(AS) -o $@ $(AFLAGS) $(*).s
@@ -63,8 +55,8 @@ lynx-stdjoy.o:
 	$(RM) $*.s
 
 lynx-160-102-16.o:
-	$(CP) $(CC65_INC)\..\tgi\$*.tgi .
-	$(CO) --code-label _lynxtgi $*.tgi
+	$(CP) $(CC65_INC)\..\tgi\lynx-160-102-16.tgi . 
+	$(CO) --code-label _lynxtgi lynx-160-102-16.tgi 
 	$(AS) -t lynx -o $@ $(AFLAGS) $*.s
 	$(RM) $*.tgi
 	$(RM) $*.s
@@ -79,4 +71,3 @@ lynx-160-102-16.o:
 	$(RM) $*.s
 	$(RM) $*.pal
 	$(RM) $*.spr
-
